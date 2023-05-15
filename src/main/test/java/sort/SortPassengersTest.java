9e7aea5a98fb3452404ee50a7bb44875dbe26c19
Akulin_Dmitry <newmitya2@gmail.com>
2023-05-15 22:31:34 +0500

    Test's fix

--- src/main/test/java/sort/SortPassengersTest.java
+++ src/main/test/java/sort/SortPassengersTest.java
@@ -1,59 +1,48 @@
-//package sort;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import transport.Passenger;
-//
-//import java.util.HashMap;
-//
-//class SortPassengersTest {
-//    private ISortPassengers sorterByAge;
-//    private ISortPassengers sorterByFinishStation;
-//    private final HashMap<String, Integer> route = new HashMap<>();
-//    private Passenger[] passengers;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        route.put("Lenina", 1);
-//        route.put("Popova", 2);
-//        route.put("Chapaeva", 4);
-//        route.put("Furmanova", 3);
-//
-//        sorterByAge = new SortPassengersByAge();
-//        sorterByFinishStation = new SortPassengersByFinishStation(route);
-//
-//        passengers =
-//            new Passenger[]
-//            {
-//                new Passenger(18, "Lenina", "Chapaeva"),
-//                new Passenger(10, "Lenina", "Popova"),
-//                new Passenger(20, "Lenina", "Furmanova")
-//            };
-//    }
-//
-//    @Test
-//    void sortByAgeTest() {
-//        Passenger[] expectedPassengersForAgesSort =
-//        {
-//            new Passenger(10, "Lenina", "Popova"),
-//            new Passenger(18, "Lenina", "Chapaeva"),
-//            new Passenger(20, "Lenina", "Furmanova")
-//        };
-//
-//        sorterByAge.sort(passengers);
-//        Assertions.assertArrayEquals(expectedPassengersForAgesSort, passengers, "Sorting by age failed");
-//    }
-//
-//    @Test
-//    void sortByFinishStation() {
-//        Passenger[] expectedPassengersForFinishStationSort = {
-//                new Passenger(10, "Lenina", "Popova"),
-//                new Passenger(20, "Lenina", "Furmanova"),
-//                new Passenger(18, "Lenina", "Chapaeva"),
-//        };
-//
-//        sorterByFinishStation.sort(passengers);
-//        Assertions.assertArrayEquals(expectedPassengersForFinishStationSort, passengers, "Sorting by finish station failed");
-//    }
-//}
+package sort;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import transport.Passenger;
+
+import java.util.ArrayList;
+
+class SortPassengersTest {
+    private ISortPassengers sorterByAge;
+    private ISortPassengers sorterByFinishStation;
+    private ArrayList<Passenger> passengers;
+
+    @BeforeEach
+    public void setUp() {
+        sorterByAge = new SortPassengersByAge();
+        sorterByFinishStation = new SortPassengersByFinishStation();
+
+        passengers = new ArrayList<>();
+
+        passengers.add(new Passenger(18, 1, 4));
+        passengers.add(new Passenger(10, 1, 2));
+        passengers.add(new Passenger(20, 1, 3));
+    }
+
+    @Test
+    void sortByAgeTest() {
+        ArrayList<Passenger> expectedPassengersForAgesSort = new ArrayList<>();
+        expectedPassengersForAgesSort.add(new Passenger(10, 1, 2));
+        expectedPassengersForAgesSort.add(new Passenger(18, 1, 4));
+        expectedPassengersForAgesSort.add(new Passenger(20, 1, 3));
+
+        sorterByAge.sort(passengers);
+        Assertions.assertEquals(expectedPassengersForAgesSort, passengers, "Sorting by age failed");
+    }
+
+    @Test
+    void sortByFinishStation() {
+        ArrayList<Passenger> expectedPassengersForFinishStationSort = new ArrayList<>();
+        expectedPassengersForFinishStationSort.add(new Passenger(10, 1, 2));
+        expectedPassengersForFinishStationSort.add(new Passenger(20, 1, 3));
+        expectedPassengersForFinishStationSort.add(new Passenger(18, 1, 4));
+
+        sorterByFinishStation.sort(passengers);
+        Assertions.assertEquals(expectedPassengersForFinishStationSort, passengers, "Sorting by finish station failed");
+    }
+}
