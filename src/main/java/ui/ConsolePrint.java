5a4ef171473d06d7ae75ac5bc173ef765bdea956
draginsky <cronosdversus@mail.ru>
2023-05-15 22:13:35 +0500

    Better print

--- src/main/java/ui/ConsolePrint.java
+++ src/main/java/ui/ConsolePrint.java
@@ -13,20 +13,22 @@ import java.util.Map;
  */
 public class ConsolePrint {
     public static void routePrint() {
+        System.out.println("Маршрут:");
         for (Map.Entry<Integer, String> pair : Route.route.entrySet()) {
-            System.out.println("Станция \"" + pair.getValue() + "\" - №" + pair.getKey());
+            System.out.println(" - Станция \"" + pair.getValue() + "\" - №" + pair.getKey());
         }
         System.out.println();
     }
 
     public static void stationStatePrint(Station station, ArrayList<Passenger> passengers, int numberOfSeats) {
         int count = 1;
-        System.out.println("\tПассажиры на остановке " + station.name() + ":");
+        System.out.println("Пассажиры на остановке \"" + station.name() + "\":" +
+                "\tСвободных мест осталось: " + (numberOfSeats - passengers.size()));
         for (Passenger passenger : passengers) {
             System.out.print(count++ + ". <age: " + passenger.age() +
                     ", from: " + passenger.startStationNum() +
                     ", to: " + passenger.finishStationNum() + ">\t");
         }
-        System.out.println("Свободных мест осталось: " + (numberOfSeats - passengers.size()));
+        System.out.println("\n");
     }
 }
